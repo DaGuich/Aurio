344df117d48cb5223b6487a9e94cd88ba90d9283
Matthias Gilch <matthias.gilch.mg@gmail.com>
2019-12-19 19:22:01 +0100

    Initial commit

---
+++ src/LibRet/LibRet.h
@@ -0,0 +1,6 @@
+#pragma once
+
+typedef unsigned int Ret_t;
+
+#define RET_OK          (0U)
+#define RET_NOK         (127U + 1U)
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include <LibRet.h>
+
+int main()
+{
+    Ret_t ret = RET_OK;
+    printf("Hello World!\n");
+    return 0;
+}
